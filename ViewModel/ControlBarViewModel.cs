810a3111df6a2adfda330fba3ad1ac708d16435f
bichptn <bichptn@newwave.vn>
2022-10-07 17:36:47 +0700

    first commit

---
+++ ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyKho.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        public ICommand CloseWindowModel { get; set; }
+        public ICommand MaximizeWindowModel { get; set; }
+        public ICommand MinimizeWindowModel { get; set; }
+        public ICommand MouseMoveWindowModel { get; set; }
+        public ControlBarViewModel()
+        {
+            CloseWindowModel = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
+            MaximizeWindowModel = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    if(w.WindowState != WindowState.Maximized)
+                        w.WindowState=WindowState.Maximized;
+                    else
+                        w.WindowState = WindowState.Normal;
+                }
+            });
+            MinimizeWindowModel = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    if (w.WindowState != WindowState.Minimized)
+                        w.WindowState = WindowState.Minimized;
+                    else
+                        w.WindowState = WindowState.Maximized;
+                }
+            });
+            MouseMoveWindowModel = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.DragMove();
+                }
+            });
+        }
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+
+        }
+    }
+}
